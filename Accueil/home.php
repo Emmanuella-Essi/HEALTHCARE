edcf7a60f8cc0686169fdbde4f72141d831f8ce2
emmanuellaessi391-oss <emmanuellaessi391@gmail.com>
2026-05-05 15:05:17 +0000

    push

---
+++ Accueil/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Healthcare</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    <link rel="stylesheet" href="Design.css">
+    
+
+
+
+</head>
+<body>
+    
+<div class="big page" id="home">
+  <div class="nav-bar">
+    <div class="logo">Health<span>Care</span></div>
+    <div class="land-nav-links">
+      <button class="btn btn-ghost" onclick="showLogin('patient')">Connexion</button>
+      <button class="btn btn-primary" onclick="showLogin('patient')">Inscription</button>
+    </div>
+  </div>
+  <div class="hero">
+    <div class="hero-tag"><span class="dot"></span>Plateforme de santé numérique</div>
+    <h1>Prends soin de ton corps, c'est le seul endroit que tu as pour vivre.</h1>
+    <p>Télé-expertise médicale, suivi vaccinal intelligent et carnet de santé numérique unifié pour patients, médecins et administrateurs.</p>
+
+   
+  </div>
+</div>
+    
+</body>
+</html>
+
+
+
