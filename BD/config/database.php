162dd7ba32b90ef1551dc936f5c8207d10e05254
haein1236 <sakisangare34@gmail.com>
2026-05-09 00:51:00 +0000

    message

--- BD/config/database.php
+++ BD/config/database.php
@@ -2,7 +2,7 @@
 // config/database.php
 
 define('DB_HOST',     getenv('DB_HOST')     ?: 'localhost');
-define('DB_NAME',     getenv('DB_NAME')     ?: 'teleexpertise');
+define('DB_NAME',     getenv('DB_NAME')     ?: 'healthcare');
 define('DB_USER',     getenv('DB_USER')     ?: 'root');
 define('DB_PASS',     getenv('DB_PASS')     ?: '');
 define('DB_CHARSET',  'utf8mb4');
